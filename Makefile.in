a8d3aa1d5062d69bf2cf890449c5b597693e3a4f
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2010-09-16 01:04:52 +0900

    changed comments. No code changed.

--- Makefile.in
+++ Makefile.in
@@ -1,5 +1,18 @@
+#
+# Copyright (C) 2010 Kazuyoshi Aizawa. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+
 # $Date: 2007/09/24 10:32:29 $, $Revision: 1.3 $ 
-# Kazuyoshi <admin2@whiteboard.ne.jp>
 
 CONFIGURE_FILES = Makefile config.status config.cache config.h config.log
 
--- tunctl.c
+++ tunctl.c
@@ -1,5 +1,22 @@
-/* Copyright 2002 Jeff Dike
+/* 
+ * Copyright 2002 Jeff Dike
  * Licensed under the GPL
+ */
+
+/*
+ *  This code is originally written by Jeff Dike
+ * 
+ *  Copyright (C) 2010 Kazuyoshi Aizawa. All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
  */
 
 /*
