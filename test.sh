15bf32d3f4bb0f9574d39abacd7a48ea664f124f
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2011-11-27 23:32:40 +0900

    Added sccipt to check ifconfig command

--- test.sh
+++ test.sh
@@ -5,6 +5,25 @@ do
 	for ppa in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 
 	do
 		sudo ./tunctl -t ${dev}${ppa} -b
+		if [ $? -ne 0 ]; then
+			echo "Faild."
+		fi
+
+		## See if ifconfig entry exists.
+		sudo ifconfig ${dev}${ppa} > /dev/null 2>&1
+		if [ $? -ne 0 ]; then
+			echo "Faild."
+		fi
+
 		sudo ./tunctl -d ${dev}${ppa} -b
+		if [ $? -ne 0 ]; then
+			echo "Faild."
+		fi
+
+		## See if ifconfig entry was removed.
+		sudo ifconfig ${dev}${ppa} > /dev/null 2>&1
+		if [ $? -eq 0 ]; then
+			echo "Faild."
+		fi
 	done
 done
